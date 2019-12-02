1397cf5ffc56cedf53e12925e78777c5e154a3e3
bynist <bayanalbunayan@gmail.com>
2019-12-02 17:09:33 +0300

    v2

--- js/app.js
+++ js/app.js
@@ -1,69 +1,102 @@
-const player1=[]
-const player2=[]
+
 // let player 
 
-let games = [[],[],[]]
 
 
+let winnerCond = [
+                  ["0","4","8"],
+                  ["0","3","6"],
+                  ["1","4","7"],
+                  ["2","5","8"],
+                  ["6","7","8"],
+                  ["3","4","5"],
+                  ["0","1","2"],
+                  ["6","4","2"]
+                ]
 
-let box1= $('#box1')
-let box2= $('#box2')
-let box3= $('#box3')
-let box4= $('#box4')
-let box5= $('#box5')
-let box6= $('#box6')
-let box7= $('#box7')
-let box8= $('#box8')
-let box9= $('#box9')
+      
+let winnerPlayes = [] // [0,0,0]
+
+// let box0= $('#0')
+// let box1= $('#1')
+// let box2= $('#2')
+// let box3= $('#3')
+// let box4= $('#4')
+// let box5= $('#5')
+// let box6= $('#6')
+// let box7= $('#7')
+// let box8= $('#8')
 
 let box =$('.boxeslist div h1')
 $(function (){
-    let countN =0
-    let arr=[]
-    let j=0
+    let countN = 0
+    let player1=[]
+    let player2=[]
     
     
             box.on('click',function(event){
-                if(countN<9 ){ // To count number of play
+                if(countN <9 ){ // To count number of play
                     
-                    if (countN%2 == 0){ // 
-                      //  let indX=0
-                            $(this).text ('X') 
-                            $(this).off('click')// to stopped click again in same box 
-                          countN += 1
-                        } else {
-                        //  let indX=5
-                            $(this).text ('O')
+                    if (countN %2 == 0){ // 
+
+                        $(this).text('X') 
+                         player1.push( $(this).attr('id'))
+                    //   console.log("player x : "+player1)
                            $(this).off('click')
-                        
+                           // to stopped click again in same box 
+                      
+                         countN += 1
+                        } 
+                        else {
+
+                            $(this).text('O')
+                             player2.push( $(this).attr('id'))
+                            // console.log(player2)
+                           $(this).off('click')
+                         
                            countN += 1
-                        }
+                      
+                         }
+                    
                 }
-                console.log(games)
-                return arr
+                
+                //call the checkValue function 
+                checkValue(player1,"player X")
+                checkValue(player2,"player O")
               })    
-             console.log((arr))
-            //  function chickWinner(arr1){
-            //      if (arr1[])
-            //   let x = $('this').eq(1)
-            //   console.log(x) 
-            //   let y = $('this').eq(2)
-            //   console.log(y) 
-            //  }
-            function checkValue (arrV){
+           
 
-                $("arrV").eq().click
+            function checkValue(player,name){
+                let result = true
+                if (player.length >= 3){
+                    for(let arr of winnerCond){ // FIRST LOOP FOR winnerCond
+                        winnerPlayes = []
+                        result = false
 
+                        for(let arr2 of arr){// SECOND LOOP FOR winnerCond
+                            if(player.indexOf(arr2) !== -1){
+                                winnerPlayes.push(arr2)
+                                if(winnerPlayes.length >= 3){
+                                    result = true
+                                    console.log("Player games: "+winnerPlayes)
+                                    console.log( name+" Winner")
+                                   // $(winnerPlayes.attr('id')).css('color','blue')
+                                    $(box).off('click')
+                                   // return result
+                                }
+                            }
+                        }/// END SECOND LOOP
+                         
+                      } /// END FIRST LOOP
+                     
+                } 
+               
             }
 
 
-            checkValue(arr)
+    
 
-
-                
-
-
-             
+                  
             
        
         
