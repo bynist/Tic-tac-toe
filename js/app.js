df0755c9231b7f23c29b00dbdc520a50b21c4d10
bynist <bayanalbunayan@gmail.com>
2019-12-04 16:27:18 +0300

    deploy1  version 4:23pm

--- js/app.js
+++ js/app.js
@@ -13,6 +13,9 @@ let winnerCond = [
 let winnerPlayes = [] // [0,0,0]
 let box =$('.boxeslist div h1')
 let button = $('#button')
+let score=0
+let scoreX=0
+let scoreO=0
 
 $(function (){
     let countN = 0
@@ -26,13 +29,17 @@ $(function (){
                 
                      // To count number of play
                     if (countN %2 == 0){ // 
+                       let scoreX=0
                         $(this).text('X')
                         $(this).css("color", "#989c9a") 
                          player1.push( $(this).attr('id'))
                         // console.log("player x : "+player1)
                          $(this).off('click') // to stopped click again in same box 
                          countN += 1
-                         checkValue(player1,"player X")
+                       
+                         checkValue(player1,"player X",0)
+                        
+                         
                           } 
                       else {
                             $(this).text('O')
@@ -42,8 +49,9 @@ $(function (){
                              $(this).off('click')
                              countN += 1
                              checkValue(player2,"player O")
+                            
                            } 
-                           console.log(countN)
+                          // console.log(countN)
             
                 //call the checkValue function 
               
@@ -52,7 +60,7 @@ $(function (){
                 //Tie condition 
                // console.log(result)
                   if ( countN === 9 && result === false){
-                    $('#alldiv > div.container > div.btns > div:nth-child(2) > h2').html(`<h2>
+                    $('#alldiv > div.container > div.btns > div:nth-child(2) ').html(`<h2>
                     you're Tie
                    </h2>`         
                    )
@@ -62,7 +70,7 @@ $(function (){
             $(box).on('click',playGame)// call play function 
            
             function checkValue(player,name){
-                
+               
                 if (player.length >= 3){
                     for(let arr of winnerCond){ // FIRST LOOP FOR winnerCond
                              winnerPlayes = []
@@ -71,18 +79,32 @@ $(function (){
                              
                             if(player.indexOf(arr2) !== -1){
                                 winnerPlayes.push(arr2)
-                                if(winnerPlayes.length >= 3){
+                                if(winnerPlayes.length === 3){
                                     result = true
                                     //winnerPlayes.css('')
-                                    console.log("Player games: "+winnerPlayes)
+                                   // console.log("Player games: "+winnerPlayes)
                                     winnerPlayes = winnerPlayes.map(function(id){ 
                                         return `#${id}`
                                     })
                                    
                                     $(winnerPlayes.join()).css('color','#597062')
-                                    $('#alldiv > div.container > div.btns > div:nth-child(2)').html(`<h2>
+                                    $('#alldiv > div.container > div.btns > div:nth-child(2) ').html(`<h2>
                                     ${name} winner 
                                    </h2>`)
+                                 
+                                    if (name=="player X"){
+                                        scoreX+=1
+                                       // console.log("scoreX res:" + scoreX)
+                                      $(' #scorexgit ').html(`<h2>
+                                      score of X : ${scoreX}  
+                                     </h2>`)
+                                    }else if (name=="player O"){
+                                        scoreO+=1
+                                        $(' #scorey').html(`<h2>
+                                        score of O :${scoreO}  
+                                       </h2>`)
+                                    }
+
                                   //  console.log('baya')
                                     // $(winnerPlayes).forEach(function(i)  {
                                     //    $( winnerPlayes[i]).css('color','red')
@@ -91,7 +113,7 @@ $(function (){
                                     $(box).off('click')
                                     
                                      console.log(result)
-                                     return
+                                     return 
                                 } 
                             } 
                             
@@ -103,7 +125,7 @@ $(function (){
                     
                 }  
 
-                }
+            }
                
             
 
